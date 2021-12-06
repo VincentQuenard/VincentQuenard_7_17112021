82d63f35a82e459f6aade58920e5ea49a39b665b
Quénard <vincent.quenard@free.fr>
2021-12-06 13:57:10 +0100

    mise en place page html/css restera à ajuster en fonction du contenu des listes pour les 3  dropdown

--- js/index.js
+++ js/index.js
@@ -1,2 +1,8 @@
 import { recipes } from './recipes.js';
 console.log(recipes);
+
+for (let i = 0; i < recipes.length; i++) {
+  console.log(recipes[i]);
+  const name = recipes[i].ingredients;
+  console.log(name);
+}
