b3f9ffb8f85cb888b6a43bdfea4f72510a755293
Quénard <vincent.quenard@free.fr>
2021-12-10 11:19:30 +0100

    gestion de l'ouverture et fermeture des dropdown par listener clic

--- js/index.js
+++ js/index.js
@@ -1,34 +1,54 @@
 import { recipes } from './recipes.js';
-console.log(recipes);
+//console.log(recipes);
 
 const recettes = document.querySelector('.cards');
 const dropdownIngredient = document.querySelector('.filter_ingredients_list');
+const dropdownAppliance = document.querySelector('.filter_appliance_list');
+const dropdownUtensil = document.querySelector('.filter_utensil_list');
+const filterIngredient = document.querySelector('.filter_ingredients');
+const filterAppliance = document.querySelector('.filter_appliance');
+const filterUtensil = document.querySelector('.filter_utensil');
+
+const filterIngredientsSearch = document.querySelector(
+  '.filter_ingredients_search'
+);
+const filterApplianceSearch = document.querySelector(
+  '.filter_appliance_search'
+);
+const filterUtensilSearch = document.querySelector('.filter_utensil_search');
+const filterIngredientsList = document.querySelector(
+  '.filter_ingredients_list'
+);
+const filterApplianceList = document.querySelector('.filter_appliance_list');
+const filterUtensilList = document.querySelector('.filter_utensil_list');
+const titleIngredients = document.querySelector('.title_ingredients');
+const titleAppliance = document.querySelector('.title_appliance');
+const titleUtensil = document.querySelector('.title_utensil');
+const arrow = document.querySelector('.filter_arrow');
+const arrowIngredients = document.querySelector('.arrow_ingredient');
+const arrowAppliance = document.querySelector('.arrow_appliance');
+const arrowUtensil = document.querySelector('.arrow_utensil');
 
 //On boucle pour parcourir toutes les recettes du tableau
 for (let i = 0; i < recipes.length; i++) {
-  console.log(recipes[i]);
+  //console.log(recipes[i].ustensils[0]);
 
   //on recupère le tableau des données ingrédients, quantité et unité contenus dans chaque recette
   const ingredientDetail = recipes[i].ingredients;
-  // console.log(ingredients);
 
   // On déclare une variable avec une chaine vide qu'on va remplir en fonction des données reçues dans la boucle pour afficher les ingrédients
-  let besoin = '';
+  let displayIngredients = '';
 
   // On boucle sur la partie ingrédient pour récupérer le contenu pour chaque partie de chaque recette de ingredients
   for (let j = 0; j < ingredientDetail.length; j++) {
-    // console.log(ingredientDetail[j]);
-
     //On récupère les données ingredient pour chaque recette
     const ingredient = ingredientDetail[j].ingredient;
 
     //On récupère les données quantity pour chaque recette
     const quantite = ingredientDetail[j].quantity;
-    //console.log(quantite);
 
     //On récupère les données unit pour chaque recette
     let unite = ingredientDetail[j].unit;
-    // console.log(unite);
 
     // On réduit le nombre de charactères pour les unités afin de prendre moin de place à l'affichage
     if (unite === 'grammes') {
@@ -41,13 +61,11 @@ for (let i = 0; i < recipes.length; i++) {
 
     //Les données injectées dans la partie des ingrédients sont des listes non ordonnées, on vérifie s'il y a des quantités et des unités aux ingrédients et on injecte en js en fonction
     if (quantite && unite != undefined) {
-      besoin += `<li>${ingredient} : ${quantite}${unite}</li>`;
+      displayIngredients += `<li>${ingredient} : ${quantite}${unite}</li>`;
     } else if (quantite != undefined && unite == undefined) {
-      besoin += `<li>${ingredient}: ${quantite}</li>`;
-      //  console.log(besoin);
+      displayIngredients += `<li>${ingredient}: ${quantite}</li>`;
     } else if (quantite && unite == undefined) {
-      besoin += `<li>${ingredient}</li>`;
-      //  console.log(besoin);
+      displayIngredients += `<li>${ingredient}</li>`;
     }
 
     dropdownIngredient.innerHTML += `<li>${ingredient}</li>`;
@@ -79,7 +97,7 @@ for (let i = 0; i < recipes.length; i++) {
             <div class="card_content_body">
               <div class="card_ingredient">
                 <ul class="card_ingredient_list">
-                ${besoin}
+                ${displayIngredients}
                 </ul>
               </div>
               <div class="card_description">
@@ -92,23 +110,81 @@ for (let i = 0; i < recipes.length; i++) {
         </article>`;
   }
   displayRecipes();
+  dropdownAppliance.innerHTML += `<li>${recipes[i].appliance}</li>`;
+  dropdownUtensil.innerHTML += `<li>${recipes[i].ustensils}</li>`;
 }
 
-const filterIngredient = document.querySelector('.filter_ingredients');
-const titleIngredients = document.querySelector('.title_ingredients');
-
-const filterIngredientsSearch = document.querySelector(
-  '.filter_ingredients_search'
-);
-const filterIngredientsList = document.querySelector(
-  '.filter_ingredients_list'
-);
-const rotationArrow = document.querySelector('.filter_arrow');
-
-filterIngredient.addEventListener('click', (e) => {
-  console.log(e);
-  rotationArrow.classList.add('rotate_arrow');
+//Fonctions des actions faites à l'ouverture des dropdowns
+function openLingredientList() {
+  arrowIngredients.classList.add('rotate_arrow');
   titleIngredients.classList.add('hidden');
   filterIngredientsSearch.classList.remove('hidden');
+  filterIngredient.style.width = '50%';
   filterIngredientsList.classList.remove('hidden');
+}
+
+function openApplicanceList() {
+  arrowAppliance.classList.add('rotate_arrow');
+  titleAppliance.classList.add('hidden');
+  filterApplianceSearch.classList.remove('hidden');
+  filterAppliance.style.width = '50%';
+  filterApplianceList.classList.remove('hidden');
+}
+
+function openUtensilList() {
+  arrowUtensil.classList.add('rotate_arrow');
+  titleUtensil.classList.add('hidden');
+  filterUtensilSearch.classList.remove('hidden');
+  filterUtensil.style.width = '50%';
+  filterUtensilList.classList.remove('hidden');
+}
+
+//Fonctions des actions faites à la fermeture des dropdowns, retour à l'affihage initial
+function closeLingredientList() {
+  arrowIngredients.classList.remove('rotate_arrow');
+  titleIngredients.classList.remove('hidden');
+  filterIngredientsSearch.classList.add('hidden');
+  filterIngredient.style.width = '10.625rem';
+  filterIngredientsList.classList.add('hidden');
+}
+
+function closeApplicanceList() {
+  arrow.classList.remove('rotate_arrow');
+  titleAppliance.classList.remove('hidden');
+  filterApplianceSearch.classList.add('hidden');
+  filterAppliance.style.width = '10.625rem';
+  filterApplianceList.classList.add('hidden');
+}
+
+function closeUtensilList() {
+  arrowUtensil.classList.remove('rotate_arrow');
+  titleUtensil.classList.remove('hidden');
+  filterUtensilSearch.classList.add('hidden');
+  filterUtensil.style.width = '10.625rem';
+  filterUtensilList.classList.add('hidden');
+}
+
+//Gestion des ouvertures et fermetures des dropdown
+arrowIngredients.addEventListener('click', (e) => {
+  if (arrowIngredients.classList.contains('rotate_arrow')) {
+    closeLingredientList();
+  } else {
+    openLingredientList();
+  }
+});
+
+arrowAppliance.addEventListener('click', (e) => {
+  if (arrowAppliance.classList.contains('rotate_arrow')) {
+    closeApplicanceList();
+  } else {
+    openApplicanceList();
+  }
+});
+
+arrowUtensil.addEventListener('click', (e) => {
+  if (arrowUtensil.classList.contains('rotate_arrow')) {
+    closeUtensilList();
+  } else {
+    openUtensilList();
+  }
 });
